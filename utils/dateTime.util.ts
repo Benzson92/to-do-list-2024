f0102e3c2bae6472e8f665da3b59d0c79f0d747d
Benzson92 <benzson.v@gmail.com>
2024-07-19 03:55:00 +0700

    Utility Functions

---
+++ utils/dateTime.util.ts
@@ -0,0 +1,42 @@
+import { format, parseISO } from "date-fns";
+
+export function formatDate(isoDate?: string): string {
+  if (!isoDate) {
+    return "";
+  }
+
+  return format(parseISO(isoDate), "MMMM d, yyyy");
+}
+
+export function formatTime(isoTime?: string): string {
+  if (!isoTime) {
+    return "";
+  }
+
+  return format(parseISO(isoTime), "h:mma");
+}
+
+function combineDateTime(formattedDate: string, formattedTime: string): string {
+  return `${formattedDate} at ${formattedTime}`;
+}
+
+export function formatDateTime(date: string, time: string): string {
+  try {
+    const formattedDate = formatDate(date);
+    const formattedTime = formatTime(time);
+    return combineDateTime(formattedDate, formattedTime);
+  } catch (error) {
+    console.error("Invalid date or time format", error);
+    return "Invalid date or time";
+  }
+}
+
+// export function formatDateTime(date: string, time: string): string {
+//   const dateTimeString = `${date}T${time}:00`;
+//   const dateTime = parseISO(dateTimeString);
+//   return format(dateTime, "MMMM d, yyyy 'at' h:mma");
+// }
+
+export const getCurrentFormattedDate = (): string => {
+  return format(new Date(), "MMMM dd, yyyy");
+};
